1a348f60bd30a7c7b7e1f95eaf2cda638c3099ad
oddyramadhan <ramadhan528@gmail.com>
2022-07-03 22:55:49 +0700

    Add testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import About from "./components/about";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+import Tab from "./components/tab";
 
-test("about us", () => {
-  render(<About />);
-  const linkElement = screen.getByText(/About Us/i);
+test("text blog", () => {
+  render(
+    <BrowserRouter>
+      <Tab />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/Blog/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("button click", () => {
+  render(
+    <BrowserRouter>
+      <Tab />
+    </BrowserRouter>
+  );
+  userEvent.click(screen.getByRole("button", { name: /Blog/i }));
 });
